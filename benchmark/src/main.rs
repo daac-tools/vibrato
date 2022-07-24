0b1d153dc3584eba5a81409578d95361fe87796e
kampersanda <shnsk.knd@gmail.com>
2022-07-24 16:25:05 +0900

    add bench

---
+++ benchmark/src/main.rs
@@ -0,0 +1,81 @@
+mod timer;
+
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use tinylattice::{Sentence, Tokenizer};
+
+use timer::Timer;
+
+use clap::Parser;
+
+const RUNS: usize = 10;
+const TRIALS: usize = 10;
+
+#[derive(Parser, Debug)]
+#[clap(name = "main", about = "A program.")]
+struct Args {
+    #[clap(short = 'i', long)]
+    sysdic_filename: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let mut reader = BufReader::new(File::open(args.sysdic_filename)?);
+    let dict = bincode::decode_from_std_read(&mut reader, bincode::config::standard())?;
+
+    let mut tokenizer = Tokenizer::new(&dict);
+    let lines: Vec<_> = std::io::stdin()
+        .lock()
+        .lines()
+        .map(|l| l.unwrap())
+        .collect();
+
+    let mut measure = |t: &mut Timer, s: &mut Sentence| {
+        let mut n_words = 0;
+        for _ in 0..RUNS {
+            t.start();
+            for line in &lines {
+                s.set_sentence(line);
+                tokenizer.tokenize(s);
+                n_words += s.morphs().len();
+            }
+            t.stop();
+        }
+        dbg!(n_words);
+    };
+
+    let mut t = Timer::new();
+    let mut s = Sentence::new();
+
+    // Warmup
+    t.reset();
+    measure(&mut t, &mut s);
+    println!("Warmup: {}", t.average());
+
+    let (mut min, mut max, mut avg) = (0.0, 0.0, 0.0);
+
+    for _ in 0..TRIALS {
+        t.reset();
+        measure(&mut t, &mut s);
+        t.discard_min();
+        t.discard_max();
+        min += t.min();
+        avg += t.average();
+        max += t.max();
+    }
+
+    min /= TRIALS as f64;
+    avg /= TRIALS as f64;
+    max /= TRIALS as f64;
+
+    println!("Number_of_sentences: {}", lines.len());
+    println!(
+        "Elapsed_seconds_to_tokenize_all_sentences: [{},{},{}]",
+        min, avg, max
+    );
+
+    Ok(())
+}
