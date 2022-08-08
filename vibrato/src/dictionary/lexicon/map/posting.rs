16042fed5c71c5534f5908ef5ff595199e1a800e
kampersanda <shnsk.knd@gmail.com>
2022-08-08 09:57:32 +0000

    vec<u8> -> vec<u32>

--- vibrato/src/dictionary/lexicon/map/posting.rs
+++ vibrato/src/dictionary/lexicon/map/posting.rs
@@ -1,56 +1,24 @@
-use std::ptr::NonNull;
-
 use bincode::{Decode, Encode};
 
-use crate::errors::{Result, VibratoError};
+use crate::errors::Result;
+use crate::utils::FromU32;
 
 #[derive(Decode, Encode)]
 pub struct Postings {
-    data: Vec<u8>,
+    data: Vec<u32>,
 }
 
 impl Postings {
     #[inline(always)]
-    pub fn ids(&self, i: usize) -> PostingsIter {
-        // Do NOT add debug_ for safety.
-        assert!(i < self.data.len());
-        let ptr = unsafe { self.data.as_ptr().add(i) };
-        let cnt = unsafe { usize::from(ptr.read()) + 1 };
-        // Note: If we skip this assertion, the tokenization time is improved by 5%-10%.
-        // But, this is necessary for safety. What should we do?
-        assert!(i + cnt * std::mem::size_of::<u32>() < self.data.len());
-        let data_ptr = unsafe { ptr.offset(1) as *const u32 };
-        PostingsIter {
-            data: unsafe { NonNull::new_unchecked(data_ptr as _) },
-            remaining: cnt,
-        }
-    }
-}
-
-pub struct PostingsIter {
-    data: NonNull<u32>,
-    remaining: usize,
-}
-
-impl Iterator for PostingsIter {
-    type Item = u32;
-
-    #[inline(always)]
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.remaining == 0 {
-            return None;
-        }
-        let ptr = self.data.as_ptr();
-        let val = unsafe { ptr.read_unaligned() };
-        self.data = unsafe { NonNull::new_unchecked(ptr.offset(1)) };
-        self.remaining -= 1;
-        Some(val)
+    pub fn ids<'a>(&'a self, i: usize) -> impl Iterator<Item = u32> + 'a {
+        let len = usize::from_u32(self.data[i]);
+        self.data[i + 1..i + 1 + len].iter().cloned()
     }
 }
 
 #[derive(Default)]
 pub struct PostingsBuilder {
-    data: Vec<u8>,
+    data: Vec<u32>,
 }
 
 impl PostingsBuilder {
@@ -60,17 +28,9 @@ impl PostingsBuilder {
 
     #[inline(always)]
     pub fn push(&mut self, ids: &[u32]) -> Result<usize> {
-        if !(1..=256).contains(&ids.len()) {
-            return Err(VibratoError::invalid_argument(
-                "ids",
-                "Number of ids associated with a word mustb be in [1,256]",
-            ));
-        }
         let offset = self.data.len();
-        self.data.push(u8::try_from(ids.len() - 1).unwrap());
-        for id in ids {
-            self.data.extend_from_slice(&id.to_le_bytes());
-        }
+        self.data.push(ids.len().try_into()?);
+        self.data.extend_from_slice(ids);
         Ok(offset)
     }
 
