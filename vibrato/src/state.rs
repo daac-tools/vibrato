6d9b340a087d648bd99ca155d8d358af383bf4c0
kampersanda <shnsk.knd@gmail.com>
2022-08-18 09:39:23 +0900

    fix

--- vibrato/src/state.rs
+++ vibrato/src/state.rs
@@ -8,7 +8,7 @@ use crate::tokenizer::lattice::{Lattice, Node};
 
 /// Maintainer of an input sentence and tokenized results.
 ///
-/// It also holds the internal data structures used for tokenization,
+/// It also holds the internal data structures used in tokenization,
 /// which can be reused to avoid unnecessary memory reallocation.
 pub struct State<'a> {
     pub(crate) dict: &'a Dictionary,
@@ -34,7 +34,8 @@ impl<'a> State<'a> {
     ///
     /// # Errors
     ///
-    /// When the input text includes characters more than [`MAX_SENTENCE_LENGTH`],
+    /// When the input sentence includes characters more than
+    /// [`MAX_SENTENCE_LENGTH`](crate::common::MAX_SENTENCE_LENGTH),
     /// an error will be returned.
     pub fn reset_sentence<S>(&mut self, input: S) -> Result<()>
     where
@@ -50,20 +51,20 @@ impl<'a> State<'a> {
         Ok(())
     }
 
-    /// Gets the number of tokens.
+    /// Gets the number of resultant tokens.
     #[inline(always)]
     pub fn num_tokens(&self) -> usize {
         self.top_nodes.len()
     }
 
-    /// Gets the `i`-th token.
+    /// Gets the `i`-th resultant token.
     #[inline(always)]
     pub fn token(&self, i: usize) -> Token {
         let index = self.num_tokens() - i - 1;
         Token::new(self, index)
     }
 
-    /// Creates an iterator of tokens.
+    /// Creates an iterator of resultant tokens.
     #[inline(always)]
     pub const fn token_iter(&'a self) -> TokenIter<'a> {
         TokenIter::new(self, 0)
@@ -82,18 +83,18 @@ impl<'a> State<'a> {
     ///
     /// # Panics
     ///
-    /// It will panic when [`init_connid_counter()`] has never been called.
+    /// It will panic when [`Self::init_connid_counter()`] has never been called.
     pub fn update_connid_counts(&mut self) {
         self.lattice
             .add_connid_counts(self.counter.as_mut().unwrap());
     }
 
     /// Computes the trained occurrence probabilities of connection ids,
-    /// returning those for left ids and right ids.
+    /// returning those for left- and right-ids.
     ///
     /// # Panics
     ///
-    /// It will panic when [`init_connid_counter()`] has never been called.
+    /// It will panic when [`Self::init_connid_counter()`] has never been called.
     pub fn compute_connid_probs(&self) -> (ConnIdProbs, ConnIdProbs) {
         self.counter.as_ref().unwrap().compute_probs()
     }
