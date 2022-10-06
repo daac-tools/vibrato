414be3b349c445b70a39673c067aea12c80477e0
Koichi Akabe <vbkaisetsu@gmail.com>
2022-10-06 10:24:20 +0900

    Update bincode to 2.0.0-rc.2 (#67)

--- vibrato/src/dictionary/lexicon/map/trie.rs
+++ vibrato/src/dictionary/lexicon/map/trie.rs
@@ -1,5 +1,5 @@
 use bincode::{
-    de::Decoder,
+    de::{BorrowDecode, BorrowDecoder, Decoder},
     enc::Encoder,
     error::{DecodeError, EncodeError},
     Decode, Encode,
@@ -22,6 +22,14 @@ impl Decode for Trie {
     fn decode<D: Decoder>(decoder: &mut D) -> Result<Self, DecodeError> {
         let data: Vec<u8> = Decode::decode(decoder)?;
         let (da, _) = crawdad::Trie::deserialize_from_slice(&data);
+        Ok(Self { da })
+    }
+}
+
+impl<'de> BorrowDecode<'de> for Trie {
+    fn borrow_decode<D: BorrowDecoder<'de>>(decoder: &mut D) -> Result<Self, DecodeError> {
+        let data: &[u8] = BorrowDecode::borrow_decode(decoder)?;
+        let (da, _) = crawdad::Trie::deserialize_from_slice(data);
         Ok(Self { da })
     }
 }
