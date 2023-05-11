c10f9282d1574e9f0d972d11b4ace3c3635e2f85
kampersanda <shnsk.knd@gmail.com>
2023-05-11 10:04:27 +0000

    update bincode

--- vibrato/src/common.rs
+++ vibrato/src/common.rs
@@ -6,7 +6,6 @@ pub const fn bincode_config() -> config::Configuration<LittleEndian, Fixint> {
     config::standard()
         .with_little_endian()
         .with_fixed_int_encoding()
-        .write_fixed_array_length()
 }
 
 /// The maximam length of an input sentence.
